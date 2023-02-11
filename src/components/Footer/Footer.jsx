4e1a306f3eede5b74d3c8e1133fbe6b64c60f707
Malgorzata Nowak <nowak.em@interia.eu>
2023-02-11 17:55:14 +0100

    Init commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css'
+
+export const Footer = () => {
+    return(
+        <div className="footer">
+            <h1>This is Footer</h1>
+        </div>
+    )
+}
